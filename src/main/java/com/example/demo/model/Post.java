12942c37c66f5a2dffdf3420f9b3a9d8b7d708dc
Ivan Svyatykh <Zlativansvyat@mail.ru>
2024-02-05 19:19:22 +0500

    Добавил время создание поста

--- src/main/java/com/example/demo/model/Post.java
+++ src/main/java/com/example/demo/model/Post.java
@@ -1,12 +1,20 @@
 package com.example.demo.model;
 
+import java.util.Date;
+
 public class Post {
 
     private String text;
     private Integer likes = 0;
+    private Date creationDate;
 
-    public Post(String text) {
+    public Post(String text, Date date) {
         this.text = text;
+        creationDate = date;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
     }
 
     public String getText() {
--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -3,17 +3,15 @@ package com.example.demo.service;
 import com.example.demo.model.Post;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class PostService {
 
     public List<Post> listAllPosts() {
 
-        return Arrays.asList(new Post("Hello"),
-                new Post("Good morning!"),
-                new Post("Good evening!"));
+        return Arrays.asList(new Post("Hello", new Date(2024, Calendar.FEBRUARY, 5)),
+                new Post("Good morning!", new Date(2024, Calendar.MARCH, 4)),
+                new Post("Good evening!", new Date(2024, Calendar.JANUARY, 3)));
     }
 }
